f9ded8c1c417ab04c687318bce9edde7cdb8be76
agnbart <polpumi@gmail.com>
2023-12-12 17:09:34 +0100

    back: feat: error handling

--- doggienote-api/src/error-doggienote.ts
+++ doggienote-api/src/error-doggienote.ts
@@ -1,5 +1,9 @@
 import { NotFoundException } from '@nestjs/common';
-
+/**
+ * Error description
+ * ('This dog has activity. It cannot be removed.', 403, 'dn_3')
+ * ('This dictActivity cannot be removed', 403, 'dn_4')
+ */
 export class ErrorDoggienote {
   readonly message: string;
   readonly statusCode: number;
@@ -9,5 +13,17 @@ export class ErrorDoggienote {
     this.message = message;
     this.statusCode = statusCode;
     this.error = error;
+  }
+}
+
+export class ErrorDoggienoteNotFound extends ErrorDoggienote {
+  constructor() {
+    super('No record found in the database', 404, 'dn_1');
+  };
+}
+
+export class ErrorDoggienoteNotCreated extends ErrorDoggienote {
+  constructor() {
+    super('The record was not created', 400, 'dn_2');
   }
 }
