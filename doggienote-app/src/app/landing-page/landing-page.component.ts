aa000328b1e779eb414b00128d138c0f1a1eea37
MacB71 <macb71@gmail.com>
2023-12-29 10:14:22 +0100

    feat: signin and signout

--- doggienote-app/src/app/landing-page/landing-page.component.ts
+++ doggienote-app/src/app/landing-page/landing-page.component.ts
@@ -1,14 +1,34 @@
 import { AfterViewInit, Component, ElementRef, HostListener } from '@angular/core';
 import { CardComponent } from '../ui/card/card.component';
 import { ButtonComponent } from '../ui/button/button.component';
+import { FooterComponent } from "../ui/footer/footer.component";
 
 @Component({
-  selector: 'dg-landing-page',
-  standalone: true,
-  imports: [CardComponent, ButtonComponent],
-  templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css'
+    selector: 'dg-landing-page',
+    standalone: true,
+    templateUrl: './landing-page.component.html',
+    styleUrl: './landing-page.component.css',
+    imports: [CardComponent, ButtonComponent, FooterComponent]
 })
-export class LandingPageComponent { 
-  
+export class LandingPageComponent {
+    public isActiveMobileMenu = false;
+    public isActiveUserMenu = false;
+    public isAuthUser = false;
+    constructor() {
+        
+    }
+
+    public SingInClicked() {
+        this.isAuthUser = !this.isAuthUser;
+    }
+    public SingOutClicked() {
+        this.isAuthUser = !this.isAuthUser;
+    }
+    
+    public userMenuClicked() {
+        this.isActiveUserMenu = !this.isActiveUserMenu;
+    }    
+    public mobileMenuClicked() {
+        this.isActiveMobileMenu = !this.isActiveMobileMenu;
+    }
 }
